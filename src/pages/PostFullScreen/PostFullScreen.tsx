8f710ae8a92f1f1d21686cb3f625bda83f41d119
fLORIAN <71528743+OfLORIAnO@users.noreply.github.com>
2023-10-07 16:56:55 +0300

    added textarea

--- src/pages/PostFullScreen/PostFullScreen.tsx
+++ src/pages/PostFullScreen/PostFullScreen.tsx
@@ -2,9 +2,10 @@ import React, { FC, useEffect } from 'react';
 
 import { useParams } from 'react-router-dom';
 
-import { Container, Typography } from '@mui/material';
+import { Box, Container, FilledInput, TextField, Typography } from '@mui/material';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { selectPost, selectStatusPost, setPost } from '../../redux/Slices/post';
+import SendIcon from '@mui/icons-material/Send';
 
 import Loading from '../../components/Loading/Loading';
 import ErrorLoading from '../../components/ErrorLoading';
@@ -12,8 +13,11 @@ import { selectTheme } from '../../redux/Slices/theme';
 
 import classNames from 'classnames';
 import s from './PostFullScreen.module.scss';
+import { Button } from '../../components/Button/Button';
 
 const PostFullScreen: FC = () => {
+    const [yourComment, setYourComment] = React.useState<string>('');
+    const [yourCommentError, setYourCommentError] = React.useState<string>(false);
     const dispatch = useAppDispatch();
     const id = useParams().id;
 
@@ -27,6 +31,19 @@ const PostFullScreen: FC = () => {
     const reloadPost = () => {
         console.log('Trying to reload');
         dispatch(setPost(Number(id)));
+    };
+
+    const handleComment = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const text = e.target.value;
+        const maxLength = 120;
+        if (text.length < maxLength) {
+            setYourComment(text);
+            setYourCommentError('');
+        } else {
+            setYourCommentError(
+                `Максимальное количество символоств составляет 50 ${maxLength}`
+            );
+        }
     };
 
     if (loadingStatus === 'error') {
@@ -61,12 +78,35 @@ const PostFullScreen: FC = () => {
                     })}
                 >
                     <img className={s.image} src={post.imageUrl} alt='' />
-                    <Typography color='secondary' variant='h2'>
+                    <Typography color='secondary' variant='h3'>
                         {post.title}
                     </Typography>
                     <Typography color='secondary' variant='subtitle1'>
                         {post.text}
                     </Typography>
+                    <div className={s.comments}>
+                        <Box
+                            component='form'
+                            noValidate
+                            autoComplete='off'
+                            className={s.comments}
+                        >
+                            <TextField
+                                className={s.comments__your}
+                                id='outlined-multiline-flexible'
+                                label='Multiline'
+                                multiline
+                                fullWidth
+                                onChange={(e) => handleComment(e)}
+                                value={yourComment}
+                                error={Boolean(yourCommentError)}
+                                helperText={yourCommentError}
+                            />
+                            <Button color='primary'>
+                                <SendIcon />
+                            </Button>
+                        </Box>
+                    </div>
                 </Container>
             </div>
         );
